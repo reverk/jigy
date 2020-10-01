31cb7022cfd21299a49140de714135682626440d
reverk <reverk@users.noreply.github.com>
2020-10-01 17:30:16 +0800

    Wip - Dashboard

--- resources/views/components/layouts/dashboard-layout.blade.php
+++ resources/views/components/layouts/dashboard-layout.blade.php
@@ -1,15 +1,17 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, initial-scale=1">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge">
 
     <!-- CSRF Token -->
     <meta name="csrf-token"
           content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} Backend</title>
 
     <!-- Fonts -->
     <link
@@ -20,33 +22,26 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"
             defer></script>
-    <script>
-        {{--Go to latest article--}}
-        function scrollToContent() {
-            let element = document.querySelector("#latest-article");
-            element.scrollIntoView({behavior: "smooth"});
-        }
-    </script>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}"
           rel="stylesheet">
     <link rel="stylesheet"
           href="{{asset('css/styles.css')}}">
+    <link rel="stylesheet"
+          href="{{asset('css/backend_styles.css')}}">
 
     <!-- Material Design icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
 </head>
 <body>
-
-<x-layouts.navbar/>
-
 <main>
+    <x-layouts.dashboard-navbar/>
+
     {{ $slot }}
+
+    <x-layouts.footer/>
 </main>
-
-<x-layouts.footer/>
-
-
 </body>
 </html>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,3 +1,46 @@
-<x-layouts.layout>
-    <h1>TODO: Work on backend stuffs</h1>
-</x-layouts.layout>
+{{--TODO: Add profile image
+https://blog.medhicham.com/en/blog-en/10-steps-to-get-you-on-image-profile-upload-for-laravel-5--}}
+<x-layouts.dashboard-layout>
+
+    <section class="container d-flex justify-content-between align-items-center flex-lg-row flex-column p-2 my-5">
+        <div class="d-inline-flex align-items-center">
+            <img src="{{auth()->user()->avatar}}"
+                 alt="Profile image"
+                 width=100px>
+            <div class="d-inline-flex align-items-center ml-4">
+                <span class="h2 font-weight-bold">{{auth()->user()->name}}</span>
+                <div class="dot mx-3"></div>
+                <span class="small text-muted">Edit Profile</span>
+            </div>
+        </div>
+        <div class="d-inline-flex align-items-center">
+            <a href="#"
+               class="btn">Create Article</a>
+            <div class="dropdown">
+                <i class="material-icons btn"
+                   role="button"
+                   type="button"
+                   data-toggle="dropdown">
+                    keyboard_arrow_down
+                </i>
+                <div class="dropdown-menu dropdown-menu-right"
+                     aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item"
+                       href="{{route('dashboard')}}">Dashboard</a>
+                    <a class="dropdown-item"
+                       href="#">Settings</a>
+                    <div class="dropdown-divider"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container">
+        <div class="h2 font-weight-bold py-2 mb-3">Articles</div>
+        @forelse($articles as $article)
+            <p>Title:{{ $article->title}}</p>
+        @empty
+            <x-error-post/>
+        @endforelse
+    </section>
+</x-layouts.dashboard-layout>
