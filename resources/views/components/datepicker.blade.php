72831423ca3ebf9b5fa23482948e3e6ecee8f5d9
KungCC1109 <leaf001109@gmail.com>
2020-10-26 20:17:53 +0800

    finish tags and date range

---
+++ resources/views/components/datepicker.blade.php
@@ -0,0 +1,16 @@
+<style>
+    .radius{
+        border-radius: 100px;
+    }
+</style>
+<div class="input-group border radius">
+    <div class="input-group-prepend p-2 ">
+        <span class="material-icons">calendar_today</span>
+    </div>
+    <input class="datepicker p-2 border-0 " data-date-format="mm/dd/yyyy" placeholder="{{$name}}">
+</div>
+<script>
+    $('.datepicker').datepicker({
+        startDate: '-3d'
+    });
+</script>
